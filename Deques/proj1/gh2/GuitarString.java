168a475ca419c41b1f3eadafda091b81d1a4ea7a
JasonDai8 <79498181+JasonDai8@users.noreply.github.com>
2023-07-25 15:47:02 -0400

    Deques added

---
+++ Deques/proj1/gh2/GuitarString.java
@@ -0,0 +1,45 @@
+package gh2;
+
+import deque.Deque;
+import deque.LinkedListDeque;
+
+//Note: This file will not compile until you complete the Deque implementations
+public class GuitarString {
+    /** Constants. Do not change. In case you're curious, the keyword final
+     * means the values cannot be changed at runtime. We'll discuss this and
+     * other topics in lecture on Friday. */
+    private static final int SR = 44100;      // Sampling Rate
+    private static final double DECAY = .996; // energy decay factor
+
+    /* Buffer for storing sound data. */
+    private Deque<Double> buffer;
+
+    /* Create a guitar string of the given frequency.  */
+    public GuitarString(double frequency) {
+        buffer = new LinkedListDeque<Double>();
+        for (int i = 0; i < (int) Math.round(SR / frequency); i++) {
+            buffer.addFirst(0.0);
+        }
+    }
+
+
+    /* Pluck the guitar string by replacing the buffer with white noise. */
+    public void pluck() {
+        for (int i = 0; i < buffer.size(); i++) {
+            buffer.removeLast();
+            buffer.addFirst(Math.random() - 0.5);
+        }
+    }
+
+    /* Advance the simulation one time step by performing one iteration of
+     * the Karplus-Strong algorithm.
+     */
+    public void tic() {
+        buffer.addLast((buffer.get(1) + buffer.removeFirst()) / 2 * DECAY);
+    }
+
+    /* Return the double at the front of the buffer. */
+    public double sample() {
+        return buffer.get(0);
+    }
+}
